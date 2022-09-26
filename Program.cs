ef5e335ebe0a1d3d2273b3cea055f4dc2fb9a6c5
Иван Искусков <Iskuskov@yandex.ru>
2022-09-26 22:22:18 +0500

    15 zadacha

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
-﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-456 -> 5
-782 -> 8
-918 -> 1
+6 -> да
+7 -> да
+1 -> нет
 */
-int GetNumber ()
-{
-Console.WriteLine("Введите число");
-    string firstNumberStr = Console.ReadLine();
-    int firstNumber =  Convert.ToInt32(firstNumberStr);
-    return firstNumber;
-} 
-int firstNumber = GetNumber();
 
-string str = firstNumber.ToString();
-Console.WriteLine(str[1]);
+Console.Write("Введите числовой день недели: ");
+int memNumber = Convert.ToInt32(Console.ReadLine());
+
+void TheEmtyDay (int memNumber) {
+  if (memNumber == 6 || memNumber == 7) {
+  Console.WriteLine("Этот день выходной))) да вы угадали");
+  }
+  else if (memNumber < 1 || memNumber > 7) {
+    Console.WriteLine("введите число от 1 до 7");
+  }
+  else Console.WriteLine("Это не выходной день!!");
+}
+
+TheEmtyDay(memNumber);
